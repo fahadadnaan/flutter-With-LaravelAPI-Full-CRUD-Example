9d41ca38f98607d7ff8afdc2cd0f2b9ca530b3b0
Fahad adnan <fahad_f_90@yahoo.com>
2019-12-01 23:23:23 +0300

    first commit

---
+++ lib/Controllers/databasehelper.dart
@@ -0,0 +1,146 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DatabaseHelper {
+  String serverUrl = "https://limitless-everglades-33254.herokuapp.com/api";
+  var status;
+
+  var token;
+
+  loginData(String email, String password) async {
+    String myUrl = "$serverUrl/user/login";
+    final response = await http.post(myUrl,
+        headers: {
+          'Accept': 'application/json'
+        },
+        body: {
+          "email": "$email",
+          "password": "$password"
+        });
+    status = response.body.contains('error');
+
+    var data = json.decode(response.body);
+
+    if (status) {
+      print('data : ${data["error"]}');
+    } else {
+      print('data : ${data["token"]}');
+      _save(data["token"]);
+    }
+  }
+
+  registerData(String name ,String email , String password) async{
+
+    String myUrl = "$serverUrl/user/register";
+    final response = await  http.post(myUrl,
+        headers: {
+          'Accept':'application/json'
+        },
+        body: {
+          "name": "$name",
+          "email": "$email",
+          "password" : "$password"
+        } ) ;
+    status = response.body.contains('error');
+
+    var data = json.decode(response.body);
+
+    if(status){
+      print('data : ${data["error"]}');
+    }else{
+      print('data : ${data["token"]}');
+      _save(data["token"]);
+    }
+
+  }
+
+  Future<List> getData() async{
+
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = prefs.get(key ) ?? 0;
+
+    String myUrl = "$serverUrl/books";
+    http.Response response = await http.get(myUrl,
+        headers: {
+          'Accept':'application/json',
+          'Authorization' : 'Bearer $value'
+        });
+    print(json.decode(response.body));
+    return json.decode(response.body);
+
+  }
+
+  void deleteData(int id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = prefs.get(key ) ?? 0;
+
+    String myUrl = "$serverUrl/books/$id";
+    http.delete(myUrl,
+        headers: {
+          'Accept':'application/json',
+          'Authorization' : 'Bearer $value'
+        } ).then((response){
+      print('Response status : ${response.statusCode}');
+      print('Response body : ${response.body}');
+    });
+  }
+
+
+  void addData(String name , String details) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = prefs.get(key ) ?? 0;
+
+    String myUrl = "$serverUrl/books";
+    http.post(myUrl,
+        headers: {
+          'Accept':'application/json',
+          'Authorization' : 'Bearer $value'
+        },
+        body: {
+          "name": "$name",
+          "details" : "$details"
+        }).then((response){
+      print('Response status : ${response.statusCode}');
+      print('Response body : ${response.body}');
+    });
+  }
+
+  void editData(int id,String name , String details) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = prefs.get(key ) ?? 0;
+
+    String myUrl = "https://limitless-everglades-33254.herokuapp.com/api/books/$id";
+    http.put(myUrl,
+        headers: {
+          'Accept':'application/json',
+          'Authorization' : 'Bearer $value'
+        },
+        body: {
+          "name": "$name",
+          "details" : "$details"
+        }).then((response){
+      print('Response status : ${response.statusCode}');
+      print('Response body : ${response.body}');
+    });
+  }
+
+  _save(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = token;
+    prefs.setString(key, value);
+  }
+
+
+  read() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'token';
+    final value = prefs.get(key ) ?? 0;
+    print('read : $value');
+  }
+}
